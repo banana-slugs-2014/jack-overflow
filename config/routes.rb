9a82ce0fb68315dafb785d676a1e0c80f65ec046
Harry Whelchel <harrywhelchel@gmail.com>
2014-03-13 16:26:51 -0700

    Update routes file with nested comments resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 JackOverflow::Application.routes.draw do
 
   root to: "posts#index"
-  resources :posts, except: :destroy
+  resources :posts, except: :destroy do
+    resources :comments
+  end
+  resources :users
   resources :sessions, only: [:new, :create, :destroy]
   get '/signin', to: 'sessions#new'
   delete '/signout', to: 'sessions#destroy'

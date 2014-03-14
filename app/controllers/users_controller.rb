2fcda917e34a32a30900224ba12ee44a87df2086
Mathilde Mouw <apprentice@dbc23.local>
2014-03-13 17:27:37 -0700

    Adds initial controller for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ActionController::Base
+  def index
+    @users = User.all
+  end
 
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:user])
+  end
+
+  def create
+    @user = User.new(username: params[:user][:username])
+    @user.password = params[:user][:password]
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      redirect_to :back
+    end
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 
 end

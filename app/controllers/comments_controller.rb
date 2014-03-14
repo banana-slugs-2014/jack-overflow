76fef6339bcfedf07ad479f54e7e1bf359d13ca0
Harry Whelchel <harrywhelchel@gmail.com>
2014-03-13 17:00:07 -0700

    Add controller actions for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,29 @@
 class CommentsController < ApplicationController
 
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new params[:comment]
+    redirect_to post_path(@comment.post) and return if @comment.save
+    redirect_to post_path(@comment.post), notice: 'Invalid comment!'
   end
 
   def edit
+    @comment = Comment.find params[:id]
   end
 
   def update
+    @comment = Comment.find params[:id]
+    redirect_to post_path(@comment.post) and return if @comment.update_attributes params[:comment]
+    redirect_to post_path(@comment.post), notice: 'Invalid comment!'
   end
 
   def destroy
+    @comment = Comment.find params[:id]
+    @comment.destroy
+    redirect_to :back
   end
-  
+
 end

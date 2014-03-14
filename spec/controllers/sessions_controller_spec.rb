d8654f7afca5981964d76717fbd5c06bf236d250
Johnny Wu <jonathan.x.wu@gmail.com>
2014-03-13 22:43:26 -0700

    Adds application controller testing: ** Test coverage at 96% ** Application Controller methods are tested ** Need to fix if user_id exists for current_user

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,28 +1,26 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe SessionsController do
+describe SessionsController do
 
-#   context "#new" do
-#     before(:each){ get :new}
-#     it{expect(response).to be_ok }
-#     # it "should show all users" do
-#     #   expect(assigns(:users)).to eq User.all
-#     # end
-#   end
+  # context "#new" do
+  #   before(:each){ get :new}
+  #   it{expect(response).to be_ok }
+  #   # it "should show all users" do
+  #   #   expect(assigns(:users)).to eq User.all
+  #   # end
+  # end
 
-#   context "#create" do
-#     before(:each){ post :create }
-#     it{expect(response).to be_ok }
-#     # it "should show all users" do
-#     #   expect(assigns(:users)).to eq User.all
-#     # end
-#   end
+  # context "#create" do
+  #   let(:invaliduser){ create :user, username: "test" }
+  #   before(:each){ post :create, user: invaliduser }
+  #   it{ expect(response).to render_template :new }
+  # end
 
-#   context "#destroy" do
-#     before(:each){ delete :destroy }
-#     it{expect(response).to be_ok }
-#     # it "should show all users" do
-#     #   expect(assigns(:users)).to eq User.all
-#     # end
-#   end
-# end
+  # context "#destroy" do
+  #   before(:each){ delete :destroy }
+  #   it{expect(response).to be_ok }
+  #   # it "should show all users" do
+  #   #   expect(assigns(:users)).to eq User.all
+  #   # end
+  # end
+end

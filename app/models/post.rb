207e9b4da1e7262907e75fb2a635255e5c81c4f9
Chris Woodrich <cwoodrich@gmail.com>
2014-03-15 11:45:46 -0700

    Change vote count method on Post model, add trending method algorithim to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,12 +7,12 @@ class Post < ActiveRecord::Base
   validates_presence_of :body
 
   def vote_count
-    self.votes.pluck(:value).inject(&:+)
+    votes.pluck(:value).inject(&:+)
   end
 
   def trending
     # youth * number of comments
-    ( 1/ ( Time.now - self.created_at ) ) * self.comments.count
+    ( 1/ ( Time.now - created_at ) ) * comments.count
   end
 
   def self.select_options

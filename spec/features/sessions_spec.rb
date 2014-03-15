bf8206b377166b51e5e6a411755296630aac2a64
Harry Whelchel <apprentice@dbc22.local>
2014-03-15 13:10:30 -0700

    Fix feature test for sessions

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -15,6 +15,10 @@ describe "Categories", :js => true do
 
   describe "sign out" do
     it "should sign a user out" do
+      visit sign_in_path
+      fill_in 'Username', :with => user.username
+      fill_in 'Password', :with => user.password
+      click_on 'Save Session'
       visit root_path
       click_on 'Sign Out'
       expect( page ).to_not have_content user.username

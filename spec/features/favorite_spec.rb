463d0ecaba0f3e42819d4d605eae368ef409b7fb
Chris Woodrich <cwoodrich@gmail.com>
2014-03-15 13:38:16 -0700

    Add feature spec to test 'ask jack' create post functionality

--- spec/features/favorite_spec.rb
+++ spec/features/favorite_spec.rb
@@ -3,22 +3,26 @@ require 'spec_helper'
 describe "Favorites", :js => true do
 
   let!(:user ) { FactoryGirl.create :user }
-  describe "sign in" do
-    it "should sign a user in" do
+  describe "Ask a question" do
+    it "should let a user log in and Ask Jack" do
       visit new_session_path
       fill_in 'Username', :with => user.username
       fill_in 'Password', :with => user.password
       click_on 'Save Session'
-      expect( page ).to have_content "yay"
+      click_on 'Ask Jack'
+      fill_in 'Title', :with => "Test Post!"
+      fill_in 'Body', :with => "This is a question!"
+      click_on 'Create Post'
+      expect( page ).to have_content "Test Post!"
     end
   end
 
-  describe "sign out" do
-    it "should sign a user out" do
-      visit users_path
-      click_on 'Logout'
-      expect( page ).to have_content "yay"
-    end
-  end
+  # describe "sign out" do
+  #   it "should sign a user out" do
+  #     visit users_path
+  #     click_on 'Logout'
+  #     expect( page ).to have_content "yay"
+  #   end
+  # end
 
 end

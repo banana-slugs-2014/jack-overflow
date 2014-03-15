54c2ec00fd27e3ff9131e2375ef49256a648c51d
Harry Whelchel <apprentice@dbc22.local>
2014-03-15 12:53:40 -0700

    Connect comments and creating new comments to questions and answers

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -5,19 +5,19 @@ describe "Categories", :js => true do
   let!(:user ) { FactoryGirl.create :user }
   describe "sign in" do
     it "should sign a user in" do
-      visit new_session_path
+      visit sign_in_path
       fill_in 'Username', :with => user.username
       fill_in 'Password', :with => user.password
       click_on 'Save Session'
-      expect( page ).to have_content "yay"
+      expect( page ).to have_content user.username
     end
   end
 
   describe "sign out" do
     it "should sign a user out" do
-      visit users_path
-      click_on 'Logout'
-      expect( page ).to have_content "yay"
+      visit root_path
+      click_on 'Sign Out'
+      expect( page ).to_not have_content user.username
     end
   end
 

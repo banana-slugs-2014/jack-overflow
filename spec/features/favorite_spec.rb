0c19f761e60548257b608fbb013aa77f8efe82ab
Chris Woodrich <cwoodrich@gmail.com>
2014-03-15 14:22:57 -0700

    Added integration test for user logging in seeing his/her own posts. Moved post partial into shared folder Added to User show view so a user can see posts

--- spec/features/favorite_spec.rb
+++ spec/features/favorite_spec.rb
@@ -5,16 +5,7 @@ describe "Favorites", :js => true do
   let!(:user ) { FactoryGirl.create :user }
   describe "Ask a question" do
     it "should let a user log in and Ask Jack" do
-      puts '*' * 100
-      puts page.body
-      puts '*' * 100
-
       visit new_session_path
-
-      puts '*' * 100
-      puts page.body
-      puts '*' * 100
-
       click_on "Sign In"
       fill_in 'Username', :with => user.username
       fill_in 'Password', :with => user.password
@@ -24,14 +15,33 @@ describe "Favorites", :js => true do
       fill_in 'Body', :with => "This is a question!"
       click_on 'Create Post'
       expect( page ).to have_content "Test Post!"
+      click_on "Sign Out"
+      visit new_session_path
+      click_on "Sign In"
+      fill_in 'Username', :with => "Jovanny"
+      fill_in 'Password', :with => "admin"
+      click_on 'Save Session'
+
+      click_on 'enim'
+      click_on 'Mark as Favorite'.first
+      expect( page ).to have_content("(marked as favorite)")
+
+
     end
   end
 
-  # describe "sign out" do
-  #   it "should sign a user out" do
-  #     visit users_path
-  #     click_on 'Logout'
-  #     expect( page ).to have_content "yay"
+  # describe "Ask a question" do
+  #   it "should let a user log in and Ask Jack" do
+  #     visit new_session_path
+  #     click_on "Sign In"
+  #     fill_in 'Username', :with => user.username
+  #     fill_in 'Password', :with => user.password
+  #     click_on 'Save Session'
+  #     click_on 'Ask Jack'
+  #     fill_in 'Title', :with => "Test Post!"
+  #     fill_in 'Body', :with => "This is a question!"
+  #     click_on 'Create Post'
+  #     expect( page ).to have_content "Test Post!"
   #   end
   # end
 

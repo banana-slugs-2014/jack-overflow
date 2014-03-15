082e997ff95816e3f244a1f3c83d50ec794761c1
Chris Woodrich & Phil Vargas <pair+chriswoodrich.philvargas@devbootcamp.com>
2014-03-14 17:30:43 -0700

    remove extraneous comments on the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :title, :body, :question_id, :type
-  # belongs_to :question, class_name: 'Post'
 
-  # has_many :answers, class_name: 'Post',
-  #                    foreign_key: "question_id"
   has_many :comments
   has_many :votes
   belongs_to :user

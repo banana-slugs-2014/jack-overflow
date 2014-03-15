8eb68c6da7a726bfd628de8fb5e101595103ba25
Chris Woodrich <cwoodrich@gmail.com>
2014-03-15 13:53:42 -0700

    Adds rebase to allow for favorite posts and favorite spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140314230240) do
+ActiveRecord::Schema.define(:version => 20140315191927) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20140314230240) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "type"
+    t.integer  "favorite_id"
   end
 
   create_table "users", :force => true do |t|
--- spec/features/favorite_spec.rb
+++ spec/features/favorite_spec.rb
@@ -5,9 +5,20 @@ describe "Favorites", :js => true do
   let!(:user ) { FactoryGirl.create :user }
   describe "Ask a question" do
     it "should let a user log in and Ask Jack" do
+      puts '*' * 100
+      puts page.body
+      puts '*' * 100
+
       visit new_session_path
+
+      puts '*' * 100
+      puts page.body
+      puts '*' * 100
+
+      click_on "Sign In"
       fill_in 'Username', :with => user.username
       fill_in 'Password', :with => user.password
+      click_on 'Save Session'
       click_on 'Ask Jack'
       fill_in 'Title', :with => "Test Post!"
       fill_in 'Body', :with => "This is a question!"

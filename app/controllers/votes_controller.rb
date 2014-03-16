851f2d893566c05d579831c35f36f47b11a1a7e9
Mathilde Mouw <nihaomouw@mac.com>
2014-03-16 16:44:32 -0700

    Finish voting functionality
      Vote form becomes put/update after vote is create
      Update action updates vote in database if user changes vote
      Update action renders new view asynchronously when user changes vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -10,15 +10,11 @@ class VotesController < ApplicationController
 
   def update
     p "YOU DONT BELIEVE IN MAGIC"
-    #allow change value of that user's vote fo that question
-  #   vote = User.find(session[:user_id]).votes.where(id: params[:id],  post_id: params[:post_id] ).first
-  #   if vote.value = params[:value]
-  #     vote.save
-  #     render partial: 'vote', locals: { post: vote.post }
-  #   elsif new_vote = User.find(session[:user_id]).votes.build(value: params[:value].to_i, post_id: params[:post_id])
-  #     render partial: 'vote', locals: { post: new_vote.post }
-  #   end
-  redirect_to '/'
+
+    vote = User.find(session[:user_id]).votes.where(id: params[:id],  post_id: params[:post_id] ).first
+    vote.value = params[:value]
+       vote.save
+    render partial: 'vote', locals: { post: vote.post }
   end
 
 end

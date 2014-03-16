9bc24f48a1cb5fc37bdcc204db20061bb7e40ca8
Apprentice <apprentice@dbc15.local>
2014-03-16 00:37:24 -0700

    AWESOME JQUERY RAILS

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,19 +1,16 @@
 class VotesController < ApplicationController
-  before_filter :protect_route, only: [:create, :update]
 
   def create
-    vote = current_user.votes.new(value: params[:value])
-    vote.post = Post.find(params[:post_id])
-    if vote.save
-      # render partial: 'vote_count' , locals: { post: vote.post }
-    else
-      # render partial: :errors, locals: { error: vote.errors.full_messages }
-    end
-    redirect_to post_path(vote.post)
+    # if session[:user_id]
+      vote = User.first.votes.build(value: params[:value].to_i)
+      vote.post = Post.find(params[:post_id])
+      vote.save
+    # end
+    render nothing: true
   end
 
   def update
-    vote = current_user.votes.where(id: params[:vote_id]).first
+    vote = User.find(session[:user_id]).votes.where(id: params[:vote_id]).first
     if vote.update_attributes params[:vote]
       render partial: 'vote_count', locals: { post: vote.post }
     else

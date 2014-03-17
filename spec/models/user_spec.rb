34ab2ea446da077f0f996b59a31a154220c35589
Phil Vargas <null>
2014-03-16 22:16:01 -0700

    refactor the sorting to implement active record search by time refactor the sorting by votes by descending implement username uniqueness validation refactor factory specs to reduce username taken issue implement search by title specs implement mark as favorite spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe User do
   context "validations" do
     it { should validate_presence_of :username }
+    it { should validate_uniqueness_of :username }
     it { should validate_presence_of :password_digest }
   end
 

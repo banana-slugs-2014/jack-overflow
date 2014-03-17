3c0c6d11c083fc5f8cd7f897b243d545ff06a575
Chris Woodrich <cwoodrich@gmail.com>
2014-03-16 17:07:14 -0700

    Change search route, add search partial to navbar, clean up search controller logic.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,7 @@
 class SearchesController < ApplicationController
   def search
-    @search_term = params[:search_term]
-    @results = Question.where("title LIKE ? OR body like ?", search_term , search_term)
-    #render search results
-  end
+    search_term = params[:search_term]
+    @questions = Question.where("title LIKE ? OR body like ?", search_term , search_term)
+    render 'posts/index'
+   end
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,12 +2,19 @@ require 'spec_helper'
 
 
 describe SearchesController do
-  let(:my_question){FactoryGirl.create :question}
-  let(:my_post) { FactoryGirl.create(:post) }
+
+let(:user){FactoryGirl.create :user}
+let!(:my_question){FactoryGirl.create :question}
+let!(:my_answer){FactoryGirl.create :answer, question: my_question}
+
+
   context '#search' do
-    context "A user (logged in or not) can search for questions in the JackOverflow database" do
-      get :search, search_term: question.title
-      expect(response.body).to include 'form'
+    describe "A user (logged in or not) can search for questions in the JackOverflow database" do
+      before(:each){ get :search, search_term: my_question.title }
+      it{ returns_valid_response }
+      xit "should return search results" do
+        expect(response.body).to include "#{my_question.title}"
+      end
     end
   end
 end
